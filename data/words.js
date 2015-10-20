159689aeed359c382137ec1e9ba88aba0d6f6b9c
Andrew <andrew@andrewt.net>
2015-10-20 14:20:27 +0100

    Update words.js

--- data/words.js
+++ data/words.js
@@ -1,3 +1,7 @@
-var words = 'Birthday pie, Pints o&rsquo; cream, Pork cylinders, Potato grids, Artificial bacon (Facon&#x2122;), Large macs, Sandwich casserole, Chocolate quail&rsquo;s eggs, Garlic pudding, Fluffy ruffs, Hoisin crispy owl, Pasta pillows, Bonbonbonbons, Mcfortune cookies, Discount foie gras, Egg &rsquot;n&rsquot; ham slabs, During-dinner mints, Quiches lorraine, 20 cheese omelette'.split(',').map(function (word) {
-	return word.trim();
-});
+var words = [
+	// Blurb
+	'Wastage', 'Community growing', 'Production methods', 'Global security',
+	'Resources', 'Science', 'Activism', 'Ethical eating',
+	// Others
+	'Skepticism', 'Genetic engineering', 'Debate', 'Future', 'Food'
+];
