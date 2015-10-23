11162bd9980431c853edc61cb4b0090cfab86688
Andrew <andrew@andrewt.net>
2015-10-23 16:48:00 +0100

    Added facts from MSF blog.
    http://www.manchestersciencefestival.blogspot.co.uk/2015/10/wanted-40-facts-about-future-of-food.html

--- data/facts.js
+++ data/facts.js
@@ -3,5 +3,8 @@ var facts = [
 	'Sub-Saharan Africa produces 230 million tonnes of food a year. The EU and North America waste 222 million.',
 	'For trawlers to catch a single kilogram of shrimp, 5.7kg of other species are inadvertantly caught in the same nets.',
 	'Organic waste is the second largest component of US landfills&nbsp;&mdash; the biggest producer of methane.',
-	'Around a third of household food is wasted. 61% of this is classed as &lsquo;avoidable&rsquo;.'
+	'Around a third of household food is wasted. 61% of this is classed as &lsquo;avoidable&rsquo;.',
+	'Crickets need 12 times less feed than cattle to produce the same amount of protein.',
+	'The Svalbard Global Seed Vault contains around 860,000 samples, protecting these crop varieties for the future.',
+	'Nearly 3 billion people rely on fish as a major source of protein.'
 ];
