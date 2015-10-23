0aa378e1450ce310b64b25b9bf601eeaebd9bd8e
Andrew <andrew@andrewt.net>
2015-10-23 17:01:40 +0100

    Update facts.js

--- data/facts.js
+++ data/facts.js
@@ -1,7 +1,7 @@
 var facts = [
 	'The Future Of Food',
 	'Sub-Saharan Africa produces 230 million tonnes of food a year. The EU and North America waste 222 million.',
-	'For trawlers to catch a single kilogram of shrimp, 5.7kg of other species are inadvertantly caught in the same nets.',
+	'For every kilogram of shrimp trawlers catch, 5.7kg of other species are inadvertantly caught in the same nets.',
 	'Organic waste is the second largest component of US landfills&nbsp;&mdash; the biggest producer of methane.',
 	'Around a third of household food is wasted. 61% of this is classed as &lsquo;avoidable&rsquo;.',
 	'Crickets need 12 times less feed than cattle to produce the same amount of protein.',
